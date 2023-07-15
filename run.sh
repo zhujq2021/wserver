7cecbea42e6d0c63997e70bf8a60817d1d74af20
zhujq <=>
2023-07-16 02:41:35 +0800

    v0.1

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 export USER=root
-
+mkdir -p /var/run/sshd
+mv /authorized_keys /root/.ssh/authorized_keys
+mv /id_rsa /root/.ssh/id_rsa
+mv /id_rsa.pub /root/.ssh/id_rsa.pub
+chmod 600 /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/id_rsa
+chmod 644 /root/.ssh/id_rsa.pub
+nohup /usr/sbin/sshd -D &
+echo 'PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;35;35m\]\w\[\033[00m\]\$\033[1;32;32m\] '"'" >> /root/.bashrc
 mkdir -p /v2ray
 cd /v2ray
 wget -O v2ray.zip http://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
