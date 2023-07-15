5b7af09fd9ff1b100317171a73e0e9c909e61c90
zhujq <=>
2023-07-16 02:35:37 +0800

    v0.1

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export USER=root
+
 mkdir -p /v2ray
 cd /v2ray
 wget -O v2ray.zip http://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
@@ -16,4 +17,4 @@ chmod +x v2ray
 V2RAY_VMESS_AEAD_FORCED=false nohup ./v2ray run> /dev/null 2>&1 &
 cd /
 chmod +x server
-./server
+/server 
