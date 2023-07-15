edd686cc04e990da65ad4bade387887f35871b49
zhujq <=>
2023-07-16 01:49:16 +0800

    init

--- server.go
+++ server.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-//	"io"
+	//	"io"
 	"log"
-//	"net"
+	//	"net"
 	"net/http"
+
 	"github.com/gorilla/websocket"
 )
 
@@ -13,36 +14,39 @@ const port = "80"
 var upgrader = websocket.Upgrader{}
 
 func defHandler(w http.ResponseWriter, r *http.Request) {
-  conn, err := upgrader.Upgrade(w, r, nil)
-  if err != nil {
-        log.Println(err)
-        return
-    }
-  defer conn.Close()
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
 	for {
-        // 读取消息
-        messageType, p, err := conn.ReadMessage()
-        if err != nil {
-            log.Println(err)
-            return
-        }
-        log.Println("Received message:", string(p))
+		// 读取消息
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("Received message:", string(p))
 
-        // 发送消息
-        err = conn.WriteMessage(messageType, p)
-        if err != nil {
-            log.Println(err)
-            return
-        }
-    }
+		// 发送消息
+		err = conn.WriteMessage(messageType, p)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
 }
 
 func main() {
 	log.Println("Listening...")
-/*	http.HandleFunc("/listen", lsHandler)
-	http.HandleFunc("/transmit", tsHandler)
-	http.HandleFunc("/dw", rayHandler)
-*/
-  http.HandleFunc("/", defHandler)
+	/*	http.HandleFunc("/listen", lsHandler)
+		http.HandleFunc("/transmit", tsHandler)
+		http.HandleFunc("/dw", rayHandler)
+	*/
+	http.HandleFunc("/", defHandler)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
